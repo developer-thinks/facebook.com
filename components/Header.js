048f034340299ae818c3c82cf03cd30eb1faa745
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2021-06-27 13:44:27 +0530

    middle section of navbar

--- components/Header.js
+++ components/Header.js
@@ -14,31 +14,41 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
+import HeaderIcon from './HeaderIcon';
+
 function Header() {
     return (
-        <div>
-            <div className="flex items-center">
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md" >
+            {/* left */}
+          <div className="flex items-center">
                 <Image 
                     src="https://www.logo.wine/a/logo/Facebook/Facebook-f_Logo-Blue-Logo.wine.svg"
-                    height={70}
-                    width={70}
+                    height={50}
+                    width={50}
                     layout="fixed"    
                 />
             <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                 <SearchIcon className="h-6 text-gray-600" />
             <input
-                className="flex ml-2 items-center bg-transparent focus:outline-none"
+                className="flex ml-2 items-center bg-transparent focus:outline-none flex-shrink"
                 placeholder="Search Facebook"
             />
             </div>
          </div>
 
-
-         <div>
-
+        {/* center */}
+         <div className="justify-center flex flex-grow" >
+            <div className="flex space-x-6 md:space-x-2">
+                <HeaderIcon active Icon={HomeIcon} />
+                <HeaderIcon Icon={FlagIcon} />
+                <HeaderIcon Icon={PlayIcon} />
+                <HeaderIcon Icon={ShoppingCartIcon} />
+                <HeaderIcon Icon={UserGroupIcon} />
+            </div>
 
          </div>
 
+            {/* right */}
 
          <div>
 
